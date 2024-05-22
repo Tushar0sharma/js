aa3916755ddf6aad8b08abc0893353b1e80879cd
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-05-22 12:53:08 +0000

    js

---
+++ 01_basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = 144359  ///fixed
+let accountEmail= "dvsnival@gmail.com"  //not fixed widely used
+var accountPass = "sdvndsk23"   ///not fixed and scope problem
+accountCity="vjbv"
+
+// var is not recommended to use.
+// accountId=2  //not allowed
+
+{
+    //scope of var
+}
+
+/*
+  preferred not to use var due to block scope and functional scope
+*/
+accountEmail="snvibsvl"
+accountPass="l kjnvb"
+accountCity="kjvnd"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPass,accountCity])
