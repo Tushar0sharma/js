b8d61318602f244e67e76ba414bcb34ea67e4778
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-05-23 04:09:20 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,25 @@
+let score = false
+
+// console.log(typeof (score));
+
+let valinnumber = Number(score);
+// console.log(valinnumber)
+// console.log(typeof(valinnumber));
+
+
+//  "33" = 33
+// "33and"=NaN
+// true = 1; false = 0
+
+let logged ="erbvv"
+let bool=Boolean(logged);
+console.log(bool)
+
+// 1=true;0=false
+//"" = false; "anain" = true
+
+let sum=48;
+let num=String(sum)
+
+console.log(typeof num)
+console.log(num)
