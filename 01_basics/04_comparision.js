eeb659b0cc1467b64162f0b9e9604a7f2623435b
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-05-23 04:35:48 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">3)  false
+// console.log("02"<1)  false
+
+// console.log(null>0) //false
+// console.log(null>=0)  //true
+// console.log(null==0)   //false
+// same goes with undefined but with false output everytime
+
+// === strict check with datatypes of both values
+// console.log("2"==2)  output:true
+// console.log("2"===2)  output:false
