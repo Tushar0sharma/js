fad57fa0da3c9ff71856b26bea91ae938a0a392f
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-05-23 04:25:08 +0000

    operations

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -13,7 +13,7 @@ let valinnumber = Number(score);
 
 let logged ="erbvv"
 let bool=Boolean(logged);
-console.log(bool)
+// console.log(bool)
 
 // 1=true;0=false
 //"" = false; "anain" = true
@@ -21,5 +21,37 @@ console.log(bool)
 let sum=48;
 let num=String(sum)
 
-console.log(typeof num)
-console.log(num)
+// console.log(typeof num)
+// console.log(num)
+
+/********    operations    *******/
+
+let val=2
+let negval=-val
+// console.log(negval)
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3/3);
+// console.log(3**3);
+// console.log(2%3);
+
+let str1="Tushar"
+let str2=" sharma"
+// console.log(str1+str2)
+
+// console.log("2"+2)
+// console.log(2+880+"vnrrjk")
+// console.log((2+880)*"vnrrjk")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3=1+2
+// console.log(num3)
+
+let cnt=0;
+cnt++;
+console.log(cnt);
