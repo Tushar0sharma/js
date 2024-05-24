2065c865116f647686990ae157ae988dfbb689bc
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-05-24 07:31:54 +0000

    mathandnumber

---
+++ 01_basics/07_numberAndMath.js
@@ -0,0 +1,34 @@
+const number=400;
+// console.log(number)
+
+const balance=new Number(1000)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(4))1
+
+const othernumber=123.44;
+// console.log(othernumber.toPrecision(4))
+
+
+const hundreds=10000000;
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//++++++++++++++++ MATH ++++++++++++++\\
+
+// console.log(Math.abs(-323))
+// console.log(Math.round(-32.6))
+// console.log(Math.ceil(32.1))
+// console.log(Math.floor(32.9))
+// console.log(Math.min(32.9,534,345))
+// console.log(Math.max(32.9,534,345))
+
+
+// console.log((Math.random()*10)+1)
+// console.log((Math.random()*10)+1)
+// console.log((Math.random()*10)+1)
+
+const min =10
+const max =11
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
