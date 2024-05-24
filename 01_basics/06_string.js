1a7b662dd9d8ebc46802058d32f24886937632c6
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-05-24 05:34:46 +0000

    string functions

---
+++ 01_basics/06_string.js
@@ -0,0 +1,33 @@
+const name="tushar"
+const repocnt=20
+
+// console.log(name+repocnt+" value")
+
+console.log(`hello my name is ${name} and my repocount is ${repocnt}`);
+
+const gamename=new String("Tushar-fc");
+
+// console.log(gamename[0]);
+// // console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.charAt(3));
+// console.log(gamename.indexOf('U'));
+
+// const newString =gamename.substring(0,6);
+// console.log(newString)
+
+// const newString =gamename.slice(-4,6);
+// console.log(newString)
+
+const newString="     tushar    "
+// console.log(newString.trim())
+// console.log(newString)
+
+
+const url="https://tushar.com/tushar%20sharma"
+// console.log(url.replace("%20"," "))
+
+// console.log(url.includes("tushar"))
+
+// console.log(gamename.split("-"))
