469db87bfeb69898c5469f3d9caf7fa6e5c0e6ae
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-05-24 10:48:56 +0000

    data and time

---
+++ 01_basics/08_dateAndTime.js
@@ -0,0 +1,16 @@
+//dates
+
+let  mydate=new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toJSON());
+// console.log(typeof mydate);
+
+// let mycreateddate=new Date(2006,1,20)
+
+// console.log(mycreateddate.toString())
+
+// console.log(mydate)
+console.log(mydate.getFullYear())
