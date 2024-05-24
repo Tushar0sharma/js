5948a11a61fbbff3666cd26a81ab79ba2cb6faff
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-05-24 13:38:32 +0000

    obj

---
+++ 02_basics/03_object.js
@@ -0,0 +1,34 @@
+//singleton
+//object.create
+
+//object literals
+
+const mysym= Symbol("key1")//for interview
+
+const jsuser={
+    name:"Tushar",
+    [mysym]:"mykey1",
+    "full name":"Tushar Sharma",
+    upi:"tushar@upi",
+    email:"tushar@gmail",
+    location:"Mathura"
+}
+
+// console.log(jsuser.email)
+// console.log(jsuser["email"])
+// console.log(jsuser["full name"])
+// console.log(jsuser[mysym])
+
+jsuser["email"]="Tushar@chatgpt.com"
+// Object.freeze(jsuser)
+jsuser["email"]="Tushar@msgpt.com"
+// console.log(jsuser)
+
+jsuser.greeting=function(){
+    console.log("Hello js user")
+}
+jsuser.greeting2=function(){
+    console.log(`Hello js user ${this.name}`)
+}
+jsuser.greeting();
+jsuser.greeting2();
